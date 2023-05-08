0381d4eb26a837387e2ed49787ebec7c9076bb7d
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-08 17:41:39 +0200

    feat: add styles to TripList

--- src/conponents/TripList.js
+++ src/conponents/TripList.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+//styles
+import './TripList.css';
 
 const TripList = () => {
   const [trips, setTrips] = useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost:3000/trips')
+      .then((response) => response.json())
+      .then((json) => setTrips(json));
+  }, []);
+
   console.log(trips);
 
-  fetch('http://localhost:3000/trips')
-    .then((response) => response.json())
-    .then((json) => setTrips(json));
-
-  return <div>TripList</div>;
+  return (
+    <div className='trip-list'>
+      <h2>TripList</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TripList;
