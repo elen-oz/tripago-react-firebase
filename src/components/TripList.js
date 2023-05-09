cd0f37e0dabd5ff882d75c127d2119a865c31bb3
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-09 17:59:28 +0200

    create custom fetch hook

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,19 +1,12 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState } from 'react';
+import { useFetch } from '../hooks/useFetch';
+
+//styles
 import './TripList.css';
 
 export default function TripList() {
-  const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState('http://localhost:3000/trips');
-
-  const fetchTrips = useCallback(async () => {
-    const response = await fetch(url);
-    const json = await response.json();
-    setTrips(json);
-  }, [url]);
-
-  useEffect(() => {
-    fetchTrips();
-  }, [fetchTrips]);
+  const { data: trips } = useFetch(url);
 
   console.log(trips);
 
@@ -21,12 +14,13 @@ export default function TripList() {
     <div className='trip-list'>
       <h2>Trip List</h2>
       <ul>
-        {trips.map((trip) => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
       </ul>
       <div className='filters'>
         <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
