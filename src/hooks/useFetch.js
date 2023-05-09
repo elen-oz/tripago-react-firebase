738461a51383d18b16ba0c36f2705ef13e2b4d23
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-09 18:42:27 +0200

    feat: handle network error

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,16 +3,24 @@ import { useState, useEffect } from 'react';
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPendig] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsPendig(true);
 
-      const res = await fetch(url);
-      const json = await res.json();
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
 
-      setIsPendig(false);
-      setData(json);
+        setIsPendig(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        setIsPendig(false);
+        setError('Could not fetch the data');
+        console.log(err.message);
+      }
     };
 
     fetchData();
