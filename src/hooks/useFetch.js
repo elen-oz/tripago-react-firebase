0288b0d04eed7409a1a5c0c8200a7b7d00c7c5d8
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-09 18:08:57 +0200

    feat: add pending state

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,17 +2,21 @@ import { useState, useEffect } from 'react';
 
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
+  const [isPending, setIsPendig] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsPendig(true);
+
       const res = await fetch(url);
       const json = await res.json();
 
+      setIsPendig(false);
       setData(json);
     };
 
     fetchData();
   }, [url]);
 
-  return { data };
+  return { data, isPending };
 };
